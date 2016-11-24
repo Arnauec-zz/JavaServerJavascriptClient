cdbb4a518db02f4a31fcc0f787856935d45f0b56
Arnau EC <arnauec@gmail.com>
2016-11-24 20:53:29 +0100

    Modificacions i neteja de codi.

--- src/chatweb/ChatServer.java
+++ src/chatweb/ChatServer.java
@@ -19,102 +19,99 @@ import org.java_websocket.WebSocketImpl;
 import org.java_websocket.framing.Framedata;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
+
 /**
  *
  * @author Epicur
  */
 
 public class ChatServer extends WebSocketServer {
+    
+    // ConcurrentHashMap per guardar les connexions i el seu iterador.
+    private ConcurrentHashMap<WebSocket, String> connexions = new ConcurrentHashMap<>();
+    private Iterator<String> iterator;
+    
+    // Inicialitza un servidor en el port especificat.
+    public ChatServer(int port) throws UnknownHostException {
+        super(new InetSocketAddress(port));
+    }
 
-        private ConcurrentHashMap<WebSocket, String> connexions = new ConcurrentHashMap<>();
-        private Iterator<String> iterator;
-	public ChatServer( int port ) throws UnknownHostException {
-		super( new InetSocketAddress( port ) );
-	}
-
-	public ChatServer( InetSocketAddress address ) {
-		super( address );
-	}
+    public ChatServer(InetSocketAddress address) {
+        super(address);
+    }
+    
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
         
-        public void onOpen( WebSocket conn, ClientHandshake handshake ) {
-		iterator = connexions.values().iterator();
-                while(iterator.hasNext()){
-                    conn.send("/newuser " + iterator.next());
-                }
-	}
+        // Quan s'obre una connexió iterem el HashMap i enviem a tots els usuaris existents
+        // que tenim un nou usuari connectat amb nom d'usuari xxxx
+        
+        iterator = connexions.values().iterator();
+        while (iterator.hasNext()) {
+            conn.send("/newuser " + iterator.next());
+        }
+    }
 
-	@Override
-	public void onClose( WebSocket conn, int code, String reason, boolean remote ) {
-                String str = connexions.get(conn);
-                connexions.remove(conn);
-		this.sendToAll("/deleteuser " + str);
-		System.out.println( conn + " ha sortit de la sala!" );
-	}
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        
+        // Quan es tanca una connexió, esborrem la connexió del HashMap i enviem
+        // un missatge a tots els usuaris connectats amb la comana /deleteuser xxxx
+        // per tal de que l'esborrin del seu JList i també els enviem un missatge
+        // normal informant-los que l'usuari h deixat la sala.
+        
+        String str = connexions.get(conn);
+        connexions.remove(conn);
+        this.sendToAll("/deleteuser " + str);
+        this.sendToAll("En/La " + str + " ha sortit de la sala!");
+        System.out.println("En/La " + str + " ha sortit de la sala!");
+    }
 
-	@Override
-	public void onMessage( WebSocket conn, String message ) {
-                if(message.contains("/newuser ")){
-                    String[] str = message.split(" ");
-                    connexions.put(conn, str[1]);
-                    this.sendToAll(message);
-                } else {
-                    this.sendToAll( message );                    
-                }
-		System.out.println( conn + ": " + message );
-	}
+    public void onMessage(WebSocket conn, String message) {
+        
+        // Quan rebem un missatge nou, si el missatge conté /newuser, és a dir,
+        // és una comana per afegir un usuari al HashMap, l'afegim, si no, 
+        // rebotem el missatge a tots els usuaris.
+        
+        if (message.contains("/newuser ")) {
+            String[] str = message.split(" ");
+            connexions.put(conn, str[1]);
+            this.sendToAll(message);
+        } else {
+            this.sendToAll(message);
+        }
+        System.out.println(message);
+    }
 
-	public void onFragment( WebSocket conn, Framedata fragment ) {
-		System.out.println( "fragment rebut: " + fragment );
-	}
+    public static void main(String[] args) throws InterruptedException, IOException {
 
-	public static void main( String[] args ) throws InterruptedException , IOException {
-		WebSocketImpl.DEBUG = true;
-		int port = 50000; // 843 flash policy port
-		try {
-			port = Integer.parseInt( args[ 0 ] );
-		} catch ( Exception ex ) {
-		}
-		ChatServer s = new ChatServer( port );
-		s.start();
-		System.out.println( "Hem iniciat el servidor en el port: " + s.getPort() );
+        int port = 50000; // 843 flash policy port
+        
+        // Inicialitzem el servidor
+        ChatServer s = new ChatServer(port);
+        s.start();
+        
+        System.out.println("Hem iniciat el servidor en el port: " + s.getPort());
+    }
 
-		BufferedReader sysin = new BufferedReader( new InputStreamReader( System.in ) );
-		while ( true ) {
-			String in = sysin.readLine();
-			s.sendToAll( in );
-			if( in.equals( "exit" ) ) {
-				s.stop();
-				break;
-			} else if( in.equals( "restart" ) ) {
-				s.stop();
-				s.start();
-				break;
-			}
-		}
-	}
-	@Override
-	public void onError( WebSocket conn, Exception ex ) {
-		ex.printStackTrace();
-		if( conn != null ) {
-			// some errors like port binding failed may not be assignable to a specific websocket
-		}
-	}
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+        if (conn != null) {
+            // some errors like port binding failed may not be assignable to a specific websocket
+        }
+    }
 
-	/**
-	 * Sends <var>text</var> to all currently connected WebSocket clients.
-	 * 
-	 * @param text
-	 *            The String to send across the network.
-	 * @throws InterruptedException
-	 *             When socket related I/O errors occur.
-	 */
-	public void sendToAll( String text ) {
-		Collection<WebSocket> con = connections();
-		synchronized ( con ) {
-			for( WebSocket c : con ) {
-				c.send( text );
-			}
-		}
-	}
+    /**
+     * Sends <var>text</var> to all currently connected WebSocket clients.
+     *
+     * @param text The String to send across the network.
+     * @throws InterruptedException When socket related I/O errors occur.
+     */
+    public void sendToAll(String text) {
+        Collection<WebSocket> con = connections();
+        synchronized (con) {
+            for (WebSocket c : con) {
+                c.send(text);
+            }
+        }
+    }
 }
-
